6d211f790d517a52c6b2661cd557779fea17d0bd
Matthew Wang <matthewwang2020@u.northwestern.edu>
2023-10-18 21:49:09 -0400

    Setup script update

--- src/inference_server/setup.sh
+++ src/inference_server/setup.sh
@@ -1,11 +1,17 @@
+# Setting up IPFS
 wget https://dist.ipfs.tech/ipfs-update/v1.9.0/ipfs-update_v1.9.0_linux-amd64.tar.gz
-
 tar -xvzf ipfs-update_v1.9.0_linux-amd64.tar.gz
-
 cd ipfs-update
-
 sudo bash install.sh
-
 sudo ipfs-update install latest
-
 ipfs --version
+
+# Setting up Python requirements
+pip install grpc
+pip install onnxruntime
+pip install ezkl
+pip install numpy
+pip install ecdsa
+pip install transformers
+pip install hashlib
+pip install pycryptodome
