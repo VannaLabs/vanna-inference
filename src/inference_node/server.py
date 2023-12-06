0f52fa373c777e037d2a099b4f8e9a9f64dac590
Matthew Wang <matthewwang2020@u.northwestern.edu>
2023-12-06 13:35:08 -0500

    zkML initial draft

---
+++ src/inference_node/server.py
@@ -0,0 +1,113 @@
+import grpc
+import os
+import sys
+from concurrent import futures
+import inference_pb2 
+import zkinference_pb2
+import inference_pb2_grpc 
+import zkinference_pb2_grpc
+import onnxruntime
+import numpy as np
+import ast
+import config
+import ecdsa
+from ecdsa import SECP256k1
+from ecdsa.keys import SigningKey, VerifyingKey
+from transformers import pipeline, set_seed
+import hashlib
+from Crypto.Hash import keccak
+import zkml
+
+class InferenceServer(inference_pb2_grpc.InferenceServicer):
+    def RunInference(self, inferenceParams, context):
+        getModel(inferenceParams.modelHash)
+        results = self.Infer(inferenceParams.modelHash, inferenceParams.modelInput)
+        return inference_pb2.InferenceResult(tx=inferenceParams.tx, node=config.public_key_hex, value=str(results))
+
+    def RunZKInference(self, inferenceParams, context):
+        results = ZKInfer(inferenceParams.modelHash, inferenceParams.modelInput, inferenceParams.tx)
+        return zkinference_pb2.ZKInferenceResult(tx=inferenceParams.tx, node=config.public_key_hex, 
+            value=str(results[0]), proof=results[1], settings=results[4], vk=results[2], srs=results[3])
+
+    def ZKInfer(self, modelHash, modelInput, txHash):
+        results = ezklProveSingle(modelHash, txHash, True)
+        return results
+
+    def Infer(self, modelHash, modelInput):
+        session = onnxruntime.InferenceSession("./models/" + modelHash, providers=[config.execution])
+        output = session.run(curateOutputs(session), curateInputs(session, modelInput))[0]
+        results = unwrap(output)
+        return results
+
+    def RunPipeline(self, pipelineParams, context):
+        results = self.Pipeline(pipelineParams.seed, pipelineParams.pipelineName, pipelineParams.modelHash, pipelineParams.modelInput)
+        return inference_pb2.InferenceResult(tx=pipelineParams.tx, node=config.public_key_hex, value=str(results))
+
+    def Pipeline(self, seed, pipelineName, model, inputs):
+        generator = pipeline(pipelineName, model=model)
+        set_seed(int(seed))
+        return generator(inputs, max_length=50, num_return_sequences=1)[0]['generated_text'].split(".")[0]
+
+def serve(port, maxWorkers):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=maxWorkers))
+    inference_pb2_grpc.add_InferenceServicer_to_server(InferenceServer(), server)
+    server.add_insecure_port("[::]:" + str(port))
+    server.start()
+    server.wait_for_termination()
+    
+def parseInput(modelInput, typeString):
+    if "tensor" in typeString:
+        if "float" in typeString:
+            return onnxruntime.OrtValue.ortvalue_from_numpy(np.array([modelInput]).astype("float32"))
+        if "string" in typeString:
+            return onnxruntime.OrtValue.ortvalue_from_numpy(np.array([modelInput]).astype("string"))
+
+def curateInputs(session, modelInput):
+    inputs = {}
+    sessionInputs = session.get_inputs()
+    for i in range(0, len(sessionInputs)):
+        param = ast.literal_eval(modelInput)[i]
+        inputs[sessionInputs[i].name] = parseInput(param, sessionInputs[i].type)
+    return inputs
+
+def curateOutputs(session):
+    outputs = []
+    for o in session.get_outputs():
+        outputs.append(o.name)
+    return outputs
+
+def getModel(cid):
+    path = "./models/" + cid
+    if os.path.isfile(path):
+        return
+    os.system("ipfs get --output=./models/" + cid + " " + cid)
+
+def sign(privateKeyHex, message):
+    privateKey = SigningKey.from_string(bytes.fromhex(privateKeyHex), curve=SECP256k1)
+    k = keccak.new(digest_bits=256)
+    k.update(b'message')
+    signature = privateKey.sign(k.digest())
+    return signature.hex()
+
+def verify(publicKeyHex, signature, value):
+    publicKey = VerifyingKey.from_string(bytes.fromhex(publicKeyHex), curve=SECP256k1)
+    k = keccak.new(digest_bits=256)
+    k.update(bytes(value.encode()))
+    try:
+        publicKey.verify(bytes.fromhex(signature), k.digest())
+        return True
+    except ecdsa.BadSignatureError:
+        print("Signature cannot be verified")
+        return False
+
+def unwrap(outputs):
+    if outputs.__class__ != list and outputs.__class__ != np.ndarray:
+        return outputs
+    if len(outputs) == 1:
+        return unwrap(outputs[0])
+    results = []
+    for x in outputs:
+        results.append(unwrap(x))
+    return results
+
+serve(config.port, config.maxWorkers)
---
+++ src/inference_node/zkml.py
@@ -0,0 +1,122 @@
+import ezkl
+import os
+import json
+
+def ezklProveSingle(modelName, transactionID, public):
+    proofStrategy = "single"
+    execDir = "scratch/"
+    modelPath = "models/" + modelName
+    compiledModelPath = modelName + "_compiled"
+    settingsPath = execDir + transactionID + ".settings"
+    vkPath = execDir + transactionID + ".vk"
+    pkPath = execDir + transactionID + ".pk"
+    witnessPath = execDir + transactionID + ".witness"
+    dataPath = execDir + transactionID + ".input"
+    proof_path = execDir + transactionID + ".pf"
+
+    runArgs = ezkl.PyRunArgs()
+    runArgs.input_visibility = "public"
+    runArgs.output_visibility = "public"
+
+    if public:
+        runArgs.param_visibility = "kzgcommit" 
+    else:
+        runArgs.param_visibility = "private"
+
+    # zkML Setup
+    if not genSettings(modelPath, settingsPath, runArgs):
+        raise Exception("Unable to generate ezKL settings")
+
+    # TODO: calibrateSettings
+
+    if not compileModel(modelPath, compiledModelPath, settingsPath):
+        raise Exception("Unable to compile model " + str(modelPath))
+
+    srsPath = getSrs(settingsPath)
+    if not srsPath:
+        raise Exception("Unable to obtain SRS")
+
+    if not genWitness(dataPath, compiledModelPath, witnessPath):
+        raise Exception("Unable to generate witness")
+
+    if not zkSetup(compiledModelPath, vkPath, pkPath, srsPath):
+        raise Exception("Unable to setup ezKL keys")
+
+    # zkML Proof Generation
+    proof = zkProve(witnessPath, compiledModelPath, pkPath, proof_path, srsPath, proofStrategy)
+    if not proof:
+        raise Exception("Unable to generate ezKL proof")
+
+    # zkML Proof Validation
+    valid = zkVerify(proof_path, settingsPath, vkPath, srsPath)
+    if not valid:
+        raise Exception("Unable to validate ezKL proof")
+
+    vk_file = open(vkPath, "rb")
+    vk = vk_file.read() 
+    vk_file.close()
+
+    srs_file = open(srsPath, "rb")
+    srs = srs_file.read() 
+    srs_file.close()
+
+    proof = json.load(open(proof_path))
+    results = [extractOutput(witnessPath, settingsPath), proof, vk, srs, json.load(open(settingsPath))]
+
+    cleanUp([witnessPath, settingsPath, pkPath, vkPath, proof_path, compiledModelPath, dataPath])
+
+    return results
+
+def extractOutput(witnessPath, settingsPath):
+    # Convert the quantized ezkl output to float value
+    outputs = json.load(open(witnessPath))['outputs']
+    with open(settingsPath) as f:
+        settings = json.load(f)
+    ezkl_outputs = [[ezkl.vecu64_to_float(
+        outputs[i][j], settings['model_output_scales'][i]) for j in range(len(outputs[i]))] for i in range(len(outputs))]
+    return ezkl_outputs
+
+def genSettings(modelPath, settingsPath, runArgs):
+    # Generate Settings
+    if not ezkl.gen_settings(modelPath, settingsPath, py_run_args=runArgs):
+        return False
+    return True
+
+def compileModel(modelPath, compiledModelPath, settingsPath):
+    # Compile ONNX model into circuit
+    if not ezkl.compile_circuit(modelPath, compiledModelPath, settingsPath):
+        return False
+    return True
+
+def getSrs(settingsPath):
+    # Get Reference String: Acquired from perpetual powers of Tau
+    # https://github.com/privacy-scaling-explorations/perpetualpowersoftau
+    settings = json.load(open(settingsPath))
+    srsPath = "srs/" + str(settings['run_args']['logrows']) + ".srs"
+    if os.path.isfile(srsPath) or ezkl.get_srs(srsPath, settingsPath):
+        return srsPath
+    return ""
+
+def genWitness(dataPath, compiledModelPath, witnessPath):
+    if not ezkl.gen_witness(dataPath, compiledModelPath, witnessPath):
+        return False
+    return True
+
+def zkSetup(compiledModelPath, vkPath, pkPath, srsPath):
+    if not ezkl.setup(compiledModelPath, vkPath, pkPath, srsPath):
+        return False
+    return True
+
+def zkProve(witnessPath, compiledModelPath, pkPath, proof_path, srsPath, proofStrategy):
+    if not ezkl.prove(witnessPath, compiledModelPath, pkPath, proof_path, srsPath, proofStrategy):
+        return False
+    return True
+
+def zkVerify(proof_path, settingsPath, vkPath, srsPath):
+    if not ezkl.verify(proof_path, settingsPath, vkPath, srsPath):
+        return False
+    return True
+
+def cleanUp(paths):
+    for p in paths:
+        os.system("rm " + p)
